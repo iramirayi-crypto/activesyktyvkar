017a978395b74f4f99b7e7fda0b571d39b92642f
Ирина Подорова <irinapodorova@gmail.com>
2026-07-22 00:46:02 +0300

    коментарии

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "initiative",
+        "author",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+    )
+
+    search_fields = (
+        "text",
+    )
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from initiatives.models import Initiative
 
-# Create your models here.
+
+class Comment(models.Model):
+    initiative = models.ForeignKey(
+        Initiative,
+        on_delete=models.CASCADE,
+        verbose_name="Инициатива"
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор"
+    )
+
+    text = models.TextField("Комментарий")
+
+    created_at = models.DateTimeField(
+        "Дата создания",
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.author} — {self.initiative}"
