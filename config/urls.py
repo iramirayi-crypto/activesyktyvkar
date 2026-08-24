0b374b73e7c38f25d222933297967757fecb08af
Ирина Подорова <irinapodorova@gmail.com>
2026-08-24 14:15:08 +0300

    Добавлены информационные страницы и улучшено восстановление пароля

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,51 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from . import views
+
 from django.conf import settings
 from django.conf.urls.static import static
+
 
 admin.site.site_header = "Администрирование"
 admin.site.site_title = "Активный Сыктывкар"
 admin.site.index_title = "Панель управления"
-admin.site.site_url = "/" 
+admin.site.site_url = "/"
+
 
 urlpatterns = [
+
     path("", views.home, name="home"),
+
+    path(
+        "about/",
+        views.about,
+        name="about"
+    ),
+
+    path(
+        "how-to-use/",
+        views.how_to_use,
+        name="how_to_use"
+    ),
+
     path("admin/", admin.site.urls),
+
     path("initiatives/", include("initiatives.urls")),
+
     path("accounts/", include("accounts.urls")),
+
     path("comments/", include("comments.urls")),
+    
+    path(
+    "contacts/",
+    views.contacts,
+    name="contacts"
+),
+
 ]
-path("accounts/", include("accounts.urls")),
+
+
 urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
--- config/views.py
+++ config/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+def about(request):
+    return render(
+        request,
+        "about.html"
+    )
+
+def how_to_use(request):
+    return render(
+        request,
+        "how_to_use.html"
+    )
+
+def contacts(request):
+    return render(
+        request,
+        "contacts.html"
+    )
